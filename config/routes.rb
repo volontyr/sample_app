4145e32a46a7f448bc02d4932fce64215a0d9a55
alexandr <santoson17@gmail.com>
2016-06-27 00:05:06 +0300

    basic user model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'users/new'
 
+  get 'usres/new'
+
+  get 'users/new'
+
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
